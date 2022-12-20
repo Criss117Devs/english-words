885c1e44c2e60cb19ad1744434fd8177ba1917e3
Criss117Devs <nevve48@gmail.com>
2022-12-19 18:44:34 -0600

    create word - 002

--- src/models/words.js
+++ src/models/words.js
@@ -4,7 +4,7 @@ const wordsTable = "words";
 
 const createWord = async (idUser, word, meaning, adjective, noun, added, status) => {
     const userAdded = await pool.query(
-        `INSERT INTO ${wordsTable} (idUser, word, meaning, adjective, noun, added, status) VALUES (?, ?, ?, ?, ?, ?)`,
+        `INSERT INTO ${wordsTable} (idUser, word, meaning, adjective, noun, added, status) VALUES (?, ?, ?, ?, ?, ?, ?)`,
         [idUser, word, meaning, adjective, noun, added, status]
     );
     return createWord;
